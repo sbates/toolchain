7441c85737fa21c6393ddc143b3b7a503fa34c60
Sascha Bates <sascha.bates@gmail.com>
2012-02-21 13:39:00 -0600

    updating cookbook url; adding apt cookbook

--- solo/win_solo.rb
+++ solo/win_solo.rb
@@ -1,4 +1,4 @@
-log_level        :debug
+log_level        :info
 log_location     STDOUT
 
 file_cache_path   "c:/chef/cache"
@@ -6,4 +6,4 @@ file_backup_path  "c:/chef/backup"
 cache_options     ({:path => "c:/chef/cache/checksums", :skip_expires => true})
 
 cookbook_path "c:/chef/cookbooks"
-recipe_url "http://s3.amazonaws.com/Alex-SF-dev/cookbooks.tgz"
+recipe_url "http://s3.amazonaws.com/bratty_packages/cookbooks.tgz"
